42a38b119a8c272f334f3ff52a5fd5152cb9a7cb
Piotr <pmachocki@gmail.com>
2013-06-01 12:10:54 -0700

    Has excess code removed. Still working.

--- DataLogger.c
+++ DataLogger.c
@@ -48,10 +48,9 @@
 
 #define DEBUG
 
-	Tuint08 analogValue = 0x00;
-	Tuint08 digitalValue = 0x00;
-    
-    Tuint08 mode;
+Tuint08 analogValue = 0x00;
+Tuint08 digitalValue = 0x00;
+
     
 // This function runs when the OS is first initialized
 void appBoot(void)
@@ -79,32 +78,22 @@ void appBoot(void)
 void appLoop_TimerTask(void)
 {
 	Tuint08 tenHz = 0;
-	Tuint08 currMode = 0;
 	
 	while (true)
 	{
         tenHz++;
-        currMode = READ_TASK_EVENT;
         genFireEvent(READ_TASK_EVENT);
-        
-        if (tenHz % 5 == 0)
-        {
-            currMode = LOG_TASK_EVENT;
-            genFireEvent(LOG_TASK_EVENT);
-        }
-        
-        
-                
 #ifdef DEBUG
         TOGGLE_PBLED(PB0);
 #endif //DEBUG
-        if (currMode)
+        
+        if (tenHz % 5 == 0)
         {
 #ifdef DEBUG
-            TOGGLE_PBLED(PB1);
-#endif //DEBUG
-        }        
-
+           TOGGLE_PBLED(PB1);
+#endif //DEBUG 
+            genFireEvent(LOG_TASK_EVENT);
+        }
 		taskDelayFromNow(20);
 	} 
 }
@@ -129,208 +118,31 @@ void appLoop_TimerTask(void)
 
 void appLoop_ReadTask(void)
 {
-
-	Tuint08 success = 0x00;
-		
 	while (true)
 	{
     	taskWaitForEvent(READ_TASK_EVENT, 0xff);
 #ifdef DEBUG
     	TOGGLE_PDLED(PD7);
 #endif //DEBUG
-
-        
+       
     	//Get actual values here.
     	//GetSensorData(); // Writes to sensor data queue
+        
     	if (taskMutexRequestOnName(AnalogSample, defLockDoNotBlock))
         {
             analogValue++;
             taskMutexReleaseOnName(AnalogSample);
-            //TOGGLE_PBLED(PB2);
-        }            
-    	//genQueuClearOnName(AnalogSample);
-    	//genQueuWriteOnName(AnalogSample, analogValue++);
-    	//taskQueuReleaseOnName(AnalogSample);
-        //TOGGLE_PBLED(PB2);
-        //}        
-       //TOGGLE_PDLED(PD7);
+        }    
         
-        
-                
-        /*
-    	taskQueuWriteRequestOnName(DigitalSample, 1);
-    	genQueuClearOnName(DigitalSample);
-    	genQueuWriteOnName(DigitalSample, digitalValue++);
-    	taskQueuReleaseOnName(DigitalSample);
-        */
-    	//genFireEvent(LOG_TASK_EVENT);
-	}
-}
-
-#endif
-
-/*
-
-#if (preTaskDefined(CalcTask))
-
-void appLoop_CalcTask(void)
-{	
-	Tuint08 analogValue = 0x00;
-	Tuint08 digitalValue = 0x00;
-	Tuint08 analogCalc = 0x00;
-	Tuint08 digitalCalc = 0x00;
-	
-	while (true)
-	{
-		taskWaitForEvent(CALC_TASK_EVENT, 0xFFFF);
-		
-		if (taskQueuReadRequestOnName(AnalogSample, 1, 0xFFFF))
-		{
-			analogValue = genQueuReadOnName(AnalogSample);
-			taskDelayFromNow(2000); //Why are we delaying here? All these can be removed
-			taskQueuReleaseOnName(AnalogSample);
-		}
-		
-		if (taskQueuReadRequestOnName(DigitalSample, 1, 0xFFFF))
-		{
-			digitalValue = genQueuReadOnName(DigitalSample);
-			taskDelayFromNow(2000); //Why are we delaying here? All these can be removed
-			taskQueuReleaseOnName(DigitalSample);
-		}
-		
-		//Calc(); // Reads from sensor data queue, writes to averaging queue
-		analogCalc = analogValue;
-		digitalCalc = digitalValue;
-		
-		if (taskQueuWriteRequestOnName(AnalogAverage, 1, 0xFFFF))
-		{
-			genQueuWriteOnName(AnalogAverage, analogCalc);
-			taskDelayFromNow(2000); //Why are we delaying here? All these can be removed
-			taskQueuReleaseOnName(AnalogAverage);
-		}
-		
-		if (taskQueuWriteRequestOnName(DigitalAverage, 1, 0xFFFF))
-		{
-			genQueuWriteOnName(DigitalAverage, digitalCalc);
-			taskDelayFromNow(2000); //Why are we delaying here? All these can be removed
-			taskQueuReleaseOnName(DigitalAverage);
-		}
-	}
-}
-#endif
-
-// This function looks pretty similar to the ReadTask. 
-// Is there a difference between these two tasks?
-// The difference is that ultimately this line of logic will call Log whereas the logic starting with ReadTask calls the CalcTask and then stops there
-// There are two lines of logic that you can follow by looking what tasks fire events and which task wait for those events. Events are not working and thats what I was hoping you could look at.
-#if (preTaskDefined(ReadAndLogTask))
-
-void appLoop_ReadAndLogTask(void)
-{
-	Tuint08 analogValue = 0x00;
-	Tuint08 digitalValue = 0x00;
-	Tuint08 success = 0x00;
-	
-	while (true)
-	{
-		taskWaitForEvent(READ_AND_LOG_TASK_EVENT, 0xFFFF);
-		//GetSensorData(); // Writes to sensor data queue
-		
-		success = 0x00;
-		while(!success) 
+        if (taskMutexRequestOnName(DigitalSample, defLockDoNotBlock))
         {
-			if (taskQueuWriteRequestOnName(AnalogSample, 1, defLockBlockInfinite))
-			{
-				genQueuClearOnName(AnalogSample);
-				taskDelayFromNow(100);  //Why are we delaying here? All these can be removed
-				genQueuWriteOnName(AnalogSample, 0x01);
-				taskDelayFromNow(100);  //Why are we delaying here? All these can be removed
-				taskQueuReleaseOnName(AnalogSample);
-				success = 0x01;
-			}
-		}		
-		
-		if (taskQueuWriteRequestOnName(DigitalSample, 1, 0xFFFF))
-		{
-			genQueuWriteOnName(DigitalSample, digitalValue++);
-			taskDelayFromNow(100);  //Why are we delaying here? All these can be removed
-			taskQueuReleaseOnName(DigitalSample);
-		}
-		
-		genFireEvent(CALC_AND_LOG_TASK_EVENT);
-	}
-}
-#endif
-
-
-#if (preTaskDefined(CalcAndLogTask))
-
-void appLoop_CalcAndLogTask(void)
-{
-	Tuint08 analogValue = 0x00;
-	Tuint08 digitalValue = 0x00;
-	Tuint08 analogCalc = 0x00;
-	Tuint08 digitalCalc = 0x00;
-	Tuint08 success = 0x00;
-	
-	Taddress address = 256;
-	Tbyte valueOut;
-	
-	while (true)
-	{
-		if (taskWaitForEvent(CALC_AND_LOG_TASK_EVENT, 0xFFFF)) 
-        {
-		    success = 0x00;
-			while(!success) 
-            {
-				if (taskQueuReadRequestOnName(AnalogSample, 1, defLockBlockInfinite))
-				{
-					genQueuClearOnName(AnalogSample);
-					taskDelayFromNow(100); //Why are we delaying here? All these can be removed
-					analogValue = genQueuReadOnName(AnalogSample);
-					taskDelayFromNow(100); //Why are we delaying here? All these can be removed
-					taskQueuReleaseOnName(AnalogSample);
-			
-					while(!portFSWriteReady());
-					valueOut = ~analogValue;
-					portFSWriteByte(address++, valueOut);
-				
-					success = 0x01;
-				}
-			}		
-		
-			if (taskQueuReadRequestOnName(DigitalSample, 1, 0xFFFF))
-			{
-				digitalValue = genQueuReadOnName(DigitalSample);
-				taskDelayFromNow(100);  //Why are we delaying here? All these can be removed
-				taskQueuReleaseOnName(DigitalSample);
-			}
-		
-			//Calc(); // Reads from sensor data queue, writes to averaging queue
-			analogCalc = analogValue;
-			digitalCalc = digitalValue;
-		
-			if (taskQueuWriteRequestOnName(AnalogAverage, 1, 0xFFFF))
-			{
-				genQueuWriteOnName(AnalogAverage, analogCalc);
-				taskDelayFromNow(100); //Why are we delaying here? All these can be removed
-				taskQueuReleaseOnName(AnalogAverage);
-			}
-		
-			if (taskQueuWriteRequestOnName(DigitalAverage, 1, 0xFFFF))
-			{
-				genQueuWriteOnName(DigitalAverage, digitalCalc);
-				taskDelayFromNow(100); //Why are we delaying here? All these can be removed
-				taskQueuReleaseOnName(DigitalAverage);
-			}
-		
-			genFireEvent(LOG_TASK_EVENT);
-		}		
+            digitalValue = 0;
+            taskMutexReleaseOnName(DigitalSample);
+        }        
 	}
 }
 
 #endif
-*/
 
 // The logging does appear to be happening here, as it seems to be done in the 
 // task above, calc_and_log. Is this because we are still experimenting?
@@ -344,10 +156,6 @@ void appLoop_LogTask(void)
 	
 	Taddress address = 512;
 	Tbyte valueOut;
-
-	Tuint08 count = 0;
-	Tuint08 uiCharCount;
-	Tchar cChar;
 	
 	while (true)
 	{
